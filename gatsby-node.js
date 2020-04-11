49afa2b62ef8213ff2af3dd93aa964b736ae58bc
johnlindquist <johnlindquist@Johns-MacBook-Pro.local>
2020-04-11 12:23:30 -0600

    removing generated pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,64 +2,6 @@ const path = require('path')
 const _ = require('lodash')
 const axios = require('axios')
 
-exports.createPages = async ({ actions, graphql }) => {
-  const { data } = await graphql(`
-    query {
-      allPodcast {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-      allLesson {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-      allCourse {
-        edges {
-          node {
-            slug
-            id
-          }
-        }
-      }
-    }
-  `)
-
-  data.allLesson.edges.forEach(({ node: lesson }) => {
-    actions.createPage({
-      path: `/lessons/${lesson.slug}`,
-      component: path.resolve(`./src/templates/lesson/lesson-query.js`),
-      context: {
-        slug: lesson.slug,
-      },
-    })
-  })
-
-  data.allPodcast.edges.forEach(({ node: podcast }) => {
-    actions.createPage({
-      path: `/podcasts/${podcast.slug}`,
-      component: path.resolve(`./src/templates/podcast/podcast-query.js`),
-      context: {
-        slug: podcast.slug,
-      },
-    })
-  })
-
-  data.allCourse.edges.forEach(({ node: course }) => {
-    actions.createPage({
-      path: `/courses/${course.slug}`,
-      component: path.resolve(`./src/templates/course/course-query.js`),
-      context: {
-        slug: course.slug,
-      },
-    })
-  })
-}
 
 exports.onCreateWebpackConfig = ({ actions, loaders }) => {
   actions.setWebpackConfig({
@@ -73,33 +15,3 @@ exports.onCreateWebpackConfig = ({ actions, loaders }) => {
   })
 }
 
-exports.sourceNodes = async ({
-  actions,
-  createNodeId,
-  createContentDigest,
-}) => {
-  const { createNode } = actions
-
-  const todoApp = await axios(`https://egghead.io/api/v1/playlists/349783`)
-  const convertingServerlessApp = await axios(
-    `https://egghead.io/api/v1/playlists/350751`,
-  )
-
-  const collections = [todoApp.data, convertingServerlessApp.data]
-
-  collections.forEach(collection =>
-    createNode(
-      Object.assign({}, collection, {
-        id: createNodeId(`collection-${collection.id}`),
-        parent: null,
-        children: [],
-        internal: {
-          type: `collection`,
-          mediaType: `application/json`,
-          content: JSON.stringify(collection),
-          contentDigest: createContentDigest(collection),
-        },
-      }),
-    ),
-  )
-}
--- src/components/EggheadChat.js
+++ src/components/EggheadChat.js
@@ -6,6 +6,12 @@ import { MessageList, MessageInput } from 'stream-chat-react';
 import { StreamChat } from 'stream-chat';
 import { gql } from 'apollo-boost';
 import { useQuery } from "urql"
+import { Styled } from 'theme-ui'
+import Layout from '../components/layout'
+import ReactPlayer from "react-player"
+
+import { useOneGraphAuth } from "use-one-graph"
+import { createClient, Provider as UrqlProvider } from "urql"
 
 
 const EGGHEADIO_IDENTITY_QUERY = gql`
@@ -90,4 +96,111 @@ let StreamChatWrapper = ({ id, fullName, email, avatarUrl, isInstructor }) => {
 }
 
 
-export default EggheadChat
+const AuthContext = React.createContext()
+
+
+ReactPlayer.displayName = "eggheadLivePlayer"
+
+
+
+
+const IndexPage = () => {
+
+    let appId = "17479ea7-24d3-4724-ab82-b4664e5004b3"
+    let auth = useOneGraphAuth("eggheadio", appId)
+
+    let client = createClient({
+        url: "https://serve.onegraph.com/dynamic?app_id=" + appId,
+        fetchOptions: () => {
+            let token = auth.client.accessToken().accessToken
+            // console.log({ auth })
+            // console.log({ token })
+            return {
+                headers: { authorization: token ? `Bearer ${token}` : '' }
+            }
+        }
+    })
+
+
+
+    return (
+        <AuthContext.Provider value={auth}>
+            <Layout title="Live">
+                <button onClick={() => auth.login("eggheadio")}>Login with egghead.io</button>
+                <Styled.h1>
+                    egghead live
+        </Styled.h1>
+
+                <div style={{ display: "flex" }}>
+
+                    <ReactPlayer
+                        url="https://stream.mux.com/8w8a7d8kuWTthNxqQx73AvFf2OCeOpNafgtaN6U5H9U.m3u8"
+                        controls playing></ReactPlayer>
+
+                    <UrqlProvider value={client}>
+                        <EggheadChat></EggheadChat>
+                    </UrqlProvider>
+
+
+
+
+                </div>
+
+
+            </Layout>
+        </AuthContext.Provider >
+    )
+}
+
+export default IndexPage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+<ReactPlayer
+                  onPlayerInitialized={setPlayer}
+                  title={lesson.title}
+                  hls_url={hls_url}
+                  dash_url={dash_url}
+                  displaySubtitles={displaySubtitles}
+                  subtitlesUrl={subtitlesUrl}
+                  url={url}
+                  poster={poster}
+                  playing={
+                    (autoStart && !canCaptureEmail) || forcedPlaybackStart
+                  }
+                  playbackRate={playbackRate}
+                  height="100%"
+                  width="100%"
+                  location={location}
+                  onDuration={setDuration}
+                  onProgress={onVideoProgress}
+                  onEnded={onVideoEnded}
+                  onError={onVideoError}
+                  onVideoQualityChanged={onVideoQualityChanged}
+                  videoQualityCookie={toJS(videoQualityCookie)}
+                  progressFrequency={100}
+                  onSubtitleChange={onSubtitleChange}
+                />
+
+                {Boolean(videoPlayerError) && !canCaptureEmail && (
+                  <VideoPlayerError
+                    title="Possible Chrome Extension Conflict"
+                    message={allowControlPlayerErrorMessage}
+                  />
+                )}
+
+
+ */
+
